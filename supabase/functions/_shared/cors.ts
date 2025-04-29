27cb9df6f009064f77ccc76218a2504df65e7d40
jemert1992 <justin@tapp.co>
2025-04-29 12:48:15 -0400

    Delete supabase directory

--- supabase/functions/_shared/cors.ts
+++
@@ -1,6 +0,0 @@
-// Standard permissive CORS headers
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
-};
-
